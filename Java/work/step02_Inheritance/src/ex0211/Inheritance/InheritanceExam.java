1e5c0f2172636d4364b2ffef690810d6f2bd5448
Polarbear <kkamang03@gmail.com>
2026-02-11 12:35:44 +0900

    실습 template 폴더 새로 업로드

---
+++ Java/work/step02_Inheritance/src/ex0211/Inheritance/InheritanceExam.java
@@ -0,0 +1,22 @@
+package ex0211.Inheritance;
+
+class Car {
+	public String carname;
+	public int cost;
+
+	protected void printAttributes() {
+		System.out.println("carname=" + carname + "\tcost=" + cost);
+	}
+}
+
+// Car를 상속받는 EfSonata, Excel, Carnival 3개 클래스 작성	
+// 각 클래스에 인수를 받지않는 생성자 작성
+// 각 클래스의 생성자의 구현부에서 carname과 cost에 적당한 값 할당
+
+public class InheritanceExam {
+	// 메인 메소드에서
+
+	// Car, EfSonata, Excel, Carnival 네개의 객체를 생성
+	// 각 클래스에서 Car calss에 있는 printAttributes()메소드를 호출할 수 있다.
+
+}
