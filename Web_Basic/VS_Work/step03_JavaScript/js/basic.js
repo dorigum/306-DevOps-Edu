2139096a46bc43802387ec141a026fdca6fe651c
PolarBear <kkamang03@gmail.com>
2026-03-26 17:52:03 +0900

    260326 [JavaScript 실습 코드 작성]

---
+++ Web_Basic/VS_Work/step03_JavaScript/js/basic.js
@@ -0,0 +1,21 @@
+// 여기는 자바스크립트 영역입니다.
+/* 여러 줄 또는 부분 주석입니다.*/
+
+// 콘솔에 출력
+console.log("어디에 출력되나요?");
+
+// 브라우저에 출력
+document.write("<h1>난 어디에 나오니?</h1>");
+
+// 함수 작성
+function test(th) {
+  // th는 this를 전달받는 매개변수
+  // window.alert("함수입니다."); // 메시지 창
+  // h2에 CSS 적용
+  // elemnet를 찾는 방법 - id로 찾기
+  // document.getElementById("h2").style.backgroundColor = "lightyellow";
+  // document.getElementById("h2").style.color = "white";
+
+  th.style.backgroundColor = "lightyellow";
+  th.style.color = "white";
+}
