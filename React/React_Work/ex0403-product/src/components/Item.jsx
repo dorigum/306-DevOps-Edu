d1e76c866a9d6558128a27798a352c54095a8349
doyeon <kkamang03@gmail.com>
2026-04-06 00:57:09 +0900

    260406 ex0403 과제 코드 작성

---
+++ React/React_Work/ex0403-product/src/components/Item.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Item.css";
+
+export const Item = (props) => {
+  return (
+    <div className="box">
+      <img src={props.imgName} alt="수박" />
+      <h5>{props.text}</h5>
+      <span>{props.price}원</span>
+    </div>
+  );
+};
+
+export default Item;
---
+++ React/React_Work/ex0403-trip/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>하와이</li>
+        <li>스페인</li>
+        <li>베트남</li>
+      </ol>
+    </nav>
+  );
+}
+
+export default Nav;
