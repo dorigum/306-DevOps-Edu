4f6881f0d00c3f25774069074ea74a2838b2d0ea
PolarBear <kkamang03@gmail.com>
2026-06-01 19:03:04 +0900

    260601 CI CD Docker AWS 실습 정리

--- CI_CD_GitActions/work/step20-jwt-aws_final/src/components/Footer.jsx
+++ CI_CD_GitActions/work/step20-jwt-aws_final/src/components/Footer.jsx
@@ -1,14 +1,14 @@
 import "./Footer.css";
-import emotion1 from "./../assets/emotion1.png";
-const Footer = () => {
-  return (
-    <>
-      <div className="Footer">
-        <img src={emotion1} />
-        <div className="Footer_div">Programmer Doyeon!</div>
-      </div>
-    </>
-  );
-};
-
-export default Footer;
+import emotion1 from './../assets/emotion1.png';
+const Footer =()=>{
+    return (
+      <>
+        <div className="Footer">
+           <img src={emotion1} />
+           <div className="Footer_div">Programmer Heejung Jang!</div>
+        </div>    
+      </>
+    )
+  }
+  
+  export default Footer;
