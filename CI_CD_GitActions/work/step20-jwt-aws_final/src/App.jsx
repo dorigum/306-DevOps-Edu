f4c5522fd8103b5a5998766634ba9c5af6d99534
PolarBear <kkamang03@gmail.com>
2026-06-02 09:23:36 +0900

    React CI/CD

--- CI_CD_GitActions/work/step20-jwt-aws_final/src/App.jsx
+++ CI_CD_GitActions/work/step20-jwt-aws_final/src/App.jsx
@@ -16,10 +16,10 @@ import { createContext, useEffect, useState } from "react";
 export const LoginContext = createContext();
 
 function App() {
-  //로그인 여부를 판단하는 상태변수 (true이면 로그인, false 로그아웃)
+  // 로그인 여부를 판단하는 상태 변수 (true이면 로그인, false 로그아웃)
   const [isLogin, setIsLogin] = useState(false);
 
-  //컴포넌트가 mount or update 될때 로그인 여부에 따른 상태값 변경
+  //컴포넌트가 mount or update 될 때 로그인 여부에 따른 상태값 변경
   useEffect(() => {
     localStorage.getItem("id") != null ? setIsLogin(true) : setIsLogin(false);
 
