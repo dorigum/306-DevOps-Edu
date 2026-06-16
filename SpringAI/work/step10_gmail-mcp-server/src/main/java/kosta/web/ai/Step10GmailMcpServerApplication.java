ebbb97643435f0cfd8e6d78d7f85f740f401ac69
PolarBear <kkamang03@gmail.com>
2026-06-16 15:47:34 +0900

    260616 Spring AI MCP Slack Gmail 실습 정리

---
+++ SpringAI/work/step10_gmail-mcp-server/src/main/java/kosta/web/ai/Step10GmailMcpServerApplication.java
@@ -0,0 +1,13 @@
+package kosta.web.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step10GmailMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Step10GmailMcpServerApplication.class, args);
+    }
+
+}
---
+++ SpringAI/work/step10_mcp_slack_gmail/src/main/java/kosta/web/ai/tools/DateTimeTools.java
@@ -0,0 +1,25 @@
+package kosta.web.ai.tools;
+
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.stereotype.Component;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class DateTimeTools {
+    @Tool(description = "요청한 도시의 현재 날짜와 시간 정보를 제공합니다.")
+    public String getCurrentDateTime(@ToolParam(description = "TimeZone (예: Asia/Seoul)") String zonedId) {
+        System.out.println("getCurrentDateTime 메소드 LLM을 통해 Spring AI가 호출하였습니다.");
+
+        ZoneId zoneId = ZoneId.of(zonedId);
+        ZonedDateTime now = ZonedDateTime.now(zoneId);
+
+        String date = now.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+
+        return "현재 날짜: %s, 현재 시간: %s".formatted(date, time);
+    }
+}
